09be6e8841c6c29739bfc25d2aef8df803d35d1e
ishanuzzal <islam15-14475@diu.edu.bd>
2022-02-09 01:10:11 +0600

    Codeforces 474B solution added

---
+++ binary search/Codeforces 474B.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+ //freopen("input.txt","r",stdin);
+  int n;
+  cin>>n;
+  int arr[n+1];
+   cin>>arr[1];
+  for(int i=2; i<=n; i++){
+    cin>>arr[i];
+    arr[i] = arr[i]+arr[i-1];
+  }
+  sort(arr+1,arr+n+1);
+
+  cout<<endl;
+  int b;
+  cin>>b;
+  while(b--){
+    int val;
+    cin>>val;
+    int low = 1;
+    int high = n;
+    while(high-low>1){
+      int mid = (high+low)/2;
+      if(val>arr[mid]){
+        low = mid+1;
+      }
+      else high = mid;
+    }
+
+    if(val<=arr[low]) cout<<low<<endl;
+    else cout<<high<<endl;
+  }
+  
+}
