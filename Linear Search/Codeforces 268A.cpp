ea4aef2e763f63a05173042d76f62162a4956525
ishanuzzal <islam15-14475@diu.edu.bd>
2022-02-08 15:01:37 +0600

    codeForces 268A added to linear search directory

---
+++ Linear Search/Codeforces 268A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+ //freopen("input.txt","r",stdin);
+  
+ int n,count=0;
+ cin>>n;
+ int arr[n][2];
+
+ for(int i=0; i<n; i++){
+   for(int j=0; j<2; j++){
+     cin>>arr[i][j];
+   }
+ }
+
+  for(int i=0; i<n; i++){
+   for(int j=0; j<n; j++){
+     if(arr[i][0]==arr[j][1] && i!=j) count++;
+   }
+ }
+  cout<<count<<endl;
+
+}
