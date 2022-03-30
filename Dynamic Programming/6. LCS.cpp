9980ac3307805d20d807cf7504e1d183bae7fbf9
ishanuzzal <islam15-14475@diu.edu.bd>
2022-03-30 22:40:40 +0600

    dp code added

---
+++ Dynamic Programming/6. LCS.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    char str[100];
+    char str2[100];
+    cin>>str;
+    cin>>str2;
+    int l1 = strlen(str);
+    int l2 = strlen(str2);
+
+    int lcs[l1+1][l2+1];
+
+    for(int i=0; i<=l1; i++){
+        lcs[i][0] = 0;
+    }
+
+    for(int j=0; j<=l2; j++){
+        lcs[0][j]=0;
+    }
+
+    for(int i=1; i<=l1; i++){
+        for(int j=1; j<=l2; j++){
+            if(str[i-1]==str2[j-1]){
+                lcs[i][j] = 1+lcs[i-1][j-1];
+            }
+            else{
+                lcs[i][j] = max(lcs[i-1][j],lcs[i][j-1]);
+            }
+        }
+    }
+
+    cout<<lcs[l1][l2]<<endl;
+
+    
+}
