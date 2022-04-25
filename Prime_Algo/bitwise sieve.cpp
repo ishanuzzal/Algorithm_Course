20bafb98c721a420a1d39f2248611fe16e6580b7
ishanuzzal <islam15-14475@diu.edu.bd>
2022-04-25 12:05:27 +0600

    bitwise sieve prime added

---
+++ Prime_Algo/bitwise sieve.cpp
@@ -0,0 +1,109 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
+#define revfo(i,k) for(i=k; i>=0; i--)
+#define ll long long 
+#define si(x)	scanf("%d",&x)
+#define sl(x)	scanf("%lld",&x)
+#define ss(s)	scanf("%s",s)
+#define pi(x)	printf("%d\n",x)
+#define pl(x)	printf("%lld\n",x)
+#define ps(s)	printf("%s\n",s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define fr first
+#define sc second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for(auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int>	pii;
+typedef pair<ll, ll>	pl;
+typedef vector<int>		vi;
+typedef vector<ll>		vl;
+typedef vector<pii>		vpii;
+typedef vector<pl>		vpl;
+typedef vector<vi>		vvi;
+typedef vector<vl>		vvl;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim) {
+	uniform_int_distribution<int> uid(0,lim-1);
+	return uid(rang);
+}
+int mpow(int base, int exp); 
+void ipgraph(int n, int m);
+void dfs(int u, int par);
+
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+
+
+vi g[N];
+int a[N];
+
+void init(){
+
+ #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+   // freopen("output.txt", "w", stdout);
+    #endif 
+}
+
+bool Check(int N,int pos){return (bool)(N & (1<<pos));}
+ int Set(int N,int pos){  return N=N | (1<<pos);}
+
+
+int main() {
+    init();
+  const int n = 100;
+  int arr[n/32+1];
+  clr(arr);
+
+    for(int i=3; i*i<=n; i+=2){
+      if(Check(arr[i/32],i%32)==0){
+        for(int j=i*i; j<=n; j+=2*i){
+        arr[j/32] = Set(arr[j/32],j%32);
+        }
+      }
+    }
+
+    for(int i=3;i<=n;i+=2){
+     if(Check(arr[i/32],i%32)==0) pi(i);
+    }
+
+    return 0;
+}
+
+
+int mpow(int base, int exp) {
+  base %= mod;
+  int result = 1;
+  while (exp > 0) {
+    if (exp & 1) result = ((ll)result * base) % mod;
+    base = ((ll)base * base) % mod;
+    exp >>= 1;
+  }
+  return result;
+}
+
+void ipgraph(int n, int m){
+	int i, u, v;
+	while(m--){
+		cin>>u>>v;
+    u--, v--;
+		g[u].pb(v);
+		g[v].pb(u);
+	}
+}
+
+void dfs(int u, int par){
+	for(int v:g[u]){
+		if (v == par) continue;
+		dfs(v, u);
+	}
+}
