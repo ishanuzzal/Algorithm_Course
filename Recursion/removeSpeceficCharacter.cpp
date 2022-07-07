85447f4a8ea6261e43e2461e4366c1bafc553815
ishanuzzal <islam15-14475@diu.edu.bd>
2022-07-07 21:33:03 +0600

    commit

--- Recursion/removeSpeceficCharacter.cpp
+++ Recursion/removeSpeceficCharacter.cpp
@@ -8,17 +8,17 @@ void init() {
     #endif 
 }
 
-string removeChar(string s){
+string removeChar(string s,char c){
   static string ans;
   if(s.size() == 0) return ans;
-  if(s[0]!='a') ans+=s[0];
-  return removeChar(s.substr(1));
+  if(s[0]!=c) ans+=s[0];
+  return removeChar(s.substr(1),c);
 }
 
 int main(){
 
   string s = "safiqul islam uzzal";
-  string ss = removeChar(s);
+  string ss = removeChar(s,a);
   cout<<ss<<endl;
 
   return 0;
