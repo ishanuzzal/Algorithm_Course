cc6fd0564e55fedb824a8c05aaa58c847b38406f
ishanuzzal <islam15-14475@diu.edu.bd>
2022-07-14 00:54:15 +0600

    duplicate issue solved

--- Recursion/Generate Subset__itterative method.cpp
+++ Recursion/Generate Subset__itterative method.cpp
@@ -5,22 +5,31 @@ void init() {
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
    // freopen("output.txt", "w", stdout);
-    #endif 
+    #endif
 }
 
 
 
 int main(){
-  
+
   init();
   vector<vector<int>> outer;
     vector<int> temp;
     outer.push_back(temp);
-  int arr[] = {1,2,3};
-  
-  for(int i=0;i<3;i++){
-    int n = outer.size();
-    for(int j=0; j<n; j++){
+  int arr[] = {1,2,3,2};
+  sort(arr,arr+4);
+  int start = 0;
+  int end = 0;
+  for(int i=0;i<4;i++){
+
+     start = 0;
+     if(i>1 && arr[i] == arr[i-1]){
+       start = end+1;
+     }
+
+     int n = outer.size();
+     end = n-1;
+    for(int j=start; j<n; j++){
         vector<int> inner;
         inner = outer[j];
         inner.push_back(arr[i]);
@@ -36,3 +45,4 @@ int main(){
     }
   return 0;
 }
+
